e326ae2c2d587fc5d044b079f8b95ee134860be7
hafsach25 <hafsachabbab50@gmail.com>
2025-11-27 14:13:05 +0100

    Update changer_mdps.php

---
+++ BEEX/app/authentification/changer_mdps.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<!--width=device-width = largeur reelle de ecran -->
+<!-- initial-scale=1= sans zoom, echelle normal -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>changer Mot de passe – BEEX</title>
+
+<!-- Bootstrap 5 -->
+<link rel="stylesheet" href="../../bootstrap-5.3.8-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../assets/auth.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6">
+                <img src="../../assets/images/logo_beex2.png" alt="Logo BEEX" class="logo-login">
+                <h1>Bienvenue dans BEEX</h1>
+                <p>Gérez toutes vos demandes rapidement et en toute transparence.</p>
+        </div>
+
+        <div class="col-md-6">
+            <div class="reset-card">
+                <h3 class="text-center">Changer mot de passe</h3>
+                <p class="text-center ">
+                    Créez un mot de passe sécurisé et unique pour protéger votre compte.
+                </p>
+                <form>
+                    <div class="mb-3">
+                        <label class="form-label">Nouveau mot de passe</label>
+                        <input type="email" class="form-control " placeholder="$#@!23HFS%$#FS">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">confirmation de mot de passe</label>
+                        <input type="email" class="form-control " placeholder="$#@!23HFS%$#FS">
+                    </div>
+
+                    <button type="submit" class="btn btn-beex ">Enregistrer</button>
+
+
+                </form>
+            </div>
+        </div>
