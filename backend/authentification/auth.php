70d239372dd78fb02650b128f730ebffcf779689
nuugha <ouahib.n672@ucd.ac.ma>
2025-11-28 21:07:52 +0100

    a

--- backend/authentification/auth.php
+++ backend/authentification/auth.php
@@ -11,11 +11,15 @@ class Auth {
     }
 
     public function login($email, $password) {
+<<<<<<< Updated upstream
         
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
 
+=======
+    
+>>>>>>> Stashed changes
         $roles = [
             [
                 "table" => "administrateur",
